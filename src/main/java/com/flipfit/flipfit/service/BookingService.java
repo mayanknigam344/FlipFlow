311d111b6695b2c7c17747560c424caad7921ee8
Mayank <mayanknigam344@gmail.com>
2025-03-26 14:11:48 +0530

    service level changes for booking

--- src/main/java/com/flipfit/flipfit/service/BookingService.java
+++ src/main/java/com/flipfit/flipfit/service/BookingService.java
@@ -4,16 +4,28 @@ import com.flipfit.flipfit.model.Center;
 import com.flipfit.flipfit.model.WorkoutVariation;
 import com.flipfit.flipfit.model.slot.Slot;
 import com.flipfit.flipfit.model.user.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@Slf4j
 public class BookingService {
 
     @Autowired SlotService slotService;
 
     public void book(User user, Center center, WorkoutVariation workoutVariation, Slot slot) {
-        //TODO: Implement this
+        List<Slot> slotsInACenter = center.getSlots();
+        if(slotsInACenter.contains(slot)){
+            int seatsForWorkoutVariation = slotService.getSeatCountInaSlotForaWorkoutVariation(slot,workoutVariation);
+            if(seatsForWorkoutVariation>0){
+                slot.getWorkoutVariationVsSeatCount().put(workoutVariation, seatsForWorkoutVariation-1);
+            }
+           log.info("Booking is Successful");
+        }
+
 
     }
 }
