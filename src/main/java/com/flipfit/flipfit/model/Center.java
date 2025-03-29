97747220ac3840d6e108a1d528949b2567310d78
Mayank <mayanknigam344@gmail.com>
2025-03-29 23:43:25 +0530

    booking flow changes

--- src/main/java/com/flipfit/flipfit/model/Center.java
+++ src/main/java/com/flipfit/flipfit/model/Center.java
@@ -9,9 +9,15 @@ import java.util.List;
 @Getter
 @Setter
 public class Center {
-    long centerId;
+    String centerId;
     String name;
     String city;
     Location location;
     List<Slot> slots;
+
+    public Center(String centerId, String name,String city){
+        this.centerId = centerId;
+        this.name = name;
+        this.city = city;
+    }
 }
